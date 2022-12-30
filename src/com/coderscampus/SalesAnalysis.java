73d1a747999792594f46a8f2d6050b3995d34185
Ross <23426097+Rlbregant@users.noreply.github.com>
2022-12-30 15:45:19 -0700

    Refactored

--- src/com/coderscampus/SalesAnalysis.java
+++ src/com/coderscampus/SalesAnalysis.java
@@ -9,41 +9,40 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SalesAnalysis {
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("MMM-yy");
+	private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("MMM-yy");
 
-    public void generateReport(String model, List<Sales> sales, SalesData salesData) {
-        // Group the sales by year and month
-        Map<YearMonth, Integer> monthlySales = sales.stream().collect(Collectors
-                .groupingBy(sale -> YearMonth.parse(sale.date, DATE_FORMAT), Collectors.summingInt(Sales::getQuantity)));
+	public void generateReport(String model, List<Sales> sales, SalesData salesData) {
+		// Group the sales by year and month
+		Map<YearMonth, Integer> monthlySales = sales.stream().collect(Collectors.groupingBy(
+				sale -> YearMonth.parse(sale.date, DATE_FORMAT), Collectors.summingInt(Sales::getQuantity)));
 
-        System.out.println(model + " Yearly Sales Report");
-        System.out.println("---------------------------");
-        // Print the sales by year
-        Map<Integer, Integer> yearlySales = monthlySales.entrySet().stream().collect(
-                Collectors.groupingBy(entry -> entry.getKey().getYear(), Collectors.summingInt(Map.Entry::getValue)));
-        for (Map.Entry<Integer, Integer> entry : yearlySales.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
-        System.out.println();
-        // Find the best and worst month
-        Sales bestMonth = sales.stream().max(Comparator.comparingInt(Sales::getQuantity))
-                .orElseThrow(() -> new RuntimeException("No sales data found"));
-        System.out.println("The best month for " + model + " was: " + bestMonth.date);
+		System.out.println(model + " Yearly Sales Report");
+		System.out.println("---------------------------");
+		// Print the sales by year
+		Map<Integer, Integer> yearlySales = monthlySales.entrySet().stream().collect(
+				Collectors.groupingBy(entry -> entry.getKey().getYear(), Collectors.summingInt(Map.Entry::getValue)));
+		for (Map.Entry<Integer, Integer> entry : yearlySales.entrySet()) {
+			System.out.println(entry.getKey() + " -> " + entry.getValue());
+		}
+		System.out.println();
+		// Find the best and worst month
+		Sales bestMonth = sales.stream().max(Comparator.comparingInt(Sales::getQuantity))
+				.orElseThrow(() -> new RuntimeException("No sales data found"));
+		System.out.println("The best month for " + model + " was: " + bestMonth.date);
 
-        Sales worstMonth = sales.stream().min(Comparator.comparingInt(Sales::getQuantity))
-                .orElseThrow(() -> new RuntimeException("No sales data found"));
-        System.out.println("The worst month for " + model + " was: " + worstMonth.date);
-        System.out.println();
-    }
+		Sales worstMonth = sales.stream().min(Comparator.comparingInt(Sales::getQuantity))
+				.orElseThrow(() -> new RuntimeException("No sales data found"));
+		System.out.println("The worst month for " + model + " was: " + worstMonth.date);
+		System.out.println();
+	}
 
+	public static void main(String[] args) throws IOException {
+		SalesData salesData = new SalesData();
+		salesData.readData();
 
-    public static void main(String[] args) throws IOException {
-        SalesData salesData = new SalesData();
-        salesData.readData();
-
-        SalesAnalysis analysis = new SalesAnalysis();
-        analysis.generateReport("Model 3", salesData.getModel3Sales(), salesData);
-        analysis.generateReport("Model S", salesData.getModelSSales(), salesData);
-        analysis.generateReport("Model X", salesData.getModelXSales(), salesData);
-    }
+		SalesAnalysis analysis = new SalesAnalysis();
+		analysis.generateReport("Model 3", salesData.getModel3Sales(), salesData);
+		analysis.generateReport("Model S", salesData.getModelSSales(), salesData);
+		analysis.generateReport("Model X", salesData.getModelXSales(), salesData);
+	}
 }
--- src/com/coderscampus/SalesData.java
+++ src/com/coderscampus/SalesData.java
@@ -7,47 +7,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SalesData {
-    private static final String MODEL_3_FILE = "model3.csv";
-    private static final String MODEL_S_FILE = "modelS.csv";
-    private static final String MODEL_X_FILE = "modelX.csv";
+	private static final String MODEL_3_FILE = "model3.csv";
+	private static final String MODEL_S_FILE = "modelS.csv";
+	private static final String MODEL_X_FILE = "modelX.csv";
 
-    final List<Sales> model3Sales;
-    final List<Sales> modelSSales;
-    final List<Sales> modelXSales;
+	final List<Sales> model3Sales;
+	final List<Sales> modelSSales;
+	final List<Sales> modelXSales;
 
-    public SalesData() {
-        model3Sales = new ArrayList<>();
-        modelSSales = new ArrayList<>();
-        modelXSales = new ArrayList<>();
-    }
+	public SalesData() {
+		model3Sales = new ArrayList<>();
+		modelSSales = new ArrayList<>();
+		modelXSales = new ArrayList<>();
+	}
 
-    public void readData() throws IOException {
-        readSales(MODEL_3_FILE, "Model 3", model3Sales);
-        readSales(MODEL_S_FILE, "Model S", modelSSales);
-        readSales(MODEL_X_FILE, "Model X", modelXSales);
-    }
+	public void readData() throws IOException {
+		readSales(MODEL_3_FILE, "Model 3", model3Sales);
+		readSales(MODEL_S_FILE, "Model S", modelSSales);
+		readSales(MODEL_X_FILE, "Model X", modelXSales);
+	}
 
-    private void readSales(String file, String model, List<Sales> salesList) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String line = reader.readLine(); // skip the first line with the header
+	private void readSales(String file, String model, List<Sales> salesList) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line = reader.readLine(); // skip the first line with the header
 
-        while ((line = reader.readLine()) != null) {
-            String[] parts = line.split(",");
-            String date = parts[0];
-            int quantity = Integer.parseInt(parts[1]);
-            Sales sale = new Sales(date, model, quantity);
-            salesList.add(sale);
-        }
-        reader.close();
-    }
+		while ((line = reader.readLine()) != null) {
+			String[] parts = line.split(",");
+			String date = parts[0];
+			int quantity = Integer.parseInt(parts[1]);
+			Sales sale = new Sales(date, model, quantity);
+			salesList.add(sale);
+		}
+		reader.close();
+	}
 
-    public List<Sales> getModel3Sales() {
-        return model3Sales;
-    }
-    public List<Sales> getModelSSales() {
-        return modelSSales;
-    }
-    public List<Sales> getModelXSales() {
-        return modelXSales;
-    }
+	public List<Sales> getModel3Sales() {
+		return model3Sales;
+	}
+
+	public List<Sales> getModelSSales() {
+		return modelSSales;
+	}
+
+	public List<Sales> getModelXSales() {
+		return modelXSales;
+	}
 }
