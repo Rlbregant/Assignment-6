6e756203957396a40c053428ab031a76b1f7ee3c
Ross <23426097+Rlbregant@users.noreply.github.com>
2022-12-30 15:43:08 -0700

    Refactored

--- src/com/coderscampus/Sales.java
+++ src/com/coderscampus/Sales.java
@@ -1,26 +1,22 @@
 package com.coderscampus;
 
-
 public class Sales {
-	  String date;
-	  String model;
-	  int quantity;
+        String date;
+        String model;
+        int quantity;
 
-	  public Sales(String date, String model, int quantity) {
-	    this.date = date;
-	    this.model = model;
-	    this.quantity = quantity;
-	  }
-
-	  public String getDate() {
-	    return date;
-	  }
-
-	  public String getModel() {
-	    return model;
-	  }
-
-	  public int getQuantity() {
-	    return quantity;
-	  }
-	}
+        public Sales(String date, String model, int quantity) {
+            this.date = date;
+            this.model = model;
+            this.quantity = quantity;
+        }
+        public String getDate() {
+            return date;
+        }
+        public String getModel() {
+            return model;
+        }
+        public int getQuantity() {
+            return quantity;
+        }
+}
--- src/com/coderscampus/SalesAnalysis.java
+++ src/com/coderscampus/SalesAnalysis.java
@@ -9,108 +9,41 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SalesAnalysis {
-	private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("MMM-yy");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("MMM-yy");
 
-	private SalesData salesData;
+    public void generateReport(String model, List<Sales> sales, SalesData salesData) {
+        // Group the sales by year and month
+        Map<YearMonth, Integer> monthlySales = sales.stream().collect(Collectors
+                .groupingBy(sale -> YearMonth.parse(sale.date, DATE_FORMAT), Collectors.summingInt(Sales::getQuantity)));
 
-	public SalesAnalysis(SalesData salesData) {
-		this.salesData = salesData;
-	}
+        System.out.println(model + " Yearly Sales Report");
+        System.out.println("---------------------------");
+        // Print the sales by year
+        Map<Integer, Integer> yearlySales = monthlySales.entrySet().stream().collect(
+                Collectors.groupingBy(entry -> entry.getKey().getYear(), Collectors.summingInt(Map.Entry::getValue)));
+        for (Map.Entry<Integer, Integer> entry : yearlySales.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+        System.out.println();
+        // Find the best and worst month
+        Sales bestMonth = sales.stream().max(Comparator.comparingInt(Sales::getQuantity))
+                .orElseThrow(() -> new RuntimeException("No sales data found"));
+        System.out.println("The best month for " + model + " was: " + bestMonth.date);
 
-	public void generateModel3Report() {
-		List<Sales> model3Sales = salesData.getModel3Sales();
-
-		// Group the sales by year and month
-		Map<YearMonth, Integer> monthlySales = model3Sales.stream().collect(Collectors
-				.groupingBy(sale -> YearMonth.parse(sale.date, DATE_FORMAT), Collectors.summingInt(Sales::getQuantity)));
-
-		System.out.println("Model 3 Yearly Sales Report");
-		System.out.println("---------------------------");
-		// Print the sales by year
-		Map<Integer, Integer> yearlySales = monthlySales.entrySet().stream().collect(
-				Collectors.groupingBy(entry -> entry.getKey().getYear(), Collectors.summingInt(Map.Entry::getValue)));
-		for (Map.Entry<Integer, Integer> entry : yearlySales.entrySet()) {
-			System.out.println(entry.getKey() + " -> " + entry.getValue());
-			
-		}
-		System.out.println("");
-		// Find the best and worst month
-		Sales bestMonth = model3Sales.stream().max(Comparator.comparingInt(Sales::getQuantity))
-				.orElseThrow(() -> new RuntimeException("No sales data found"));
-		System.out.println("The best month for Model 3 was: " + bestMonth.date);
-
-		Sales worstMonth = model3Sales.stream().min(Comparator.comparingInt(Sales::getQuantity))
-				.orElseThrow(() -> new RuntimeException("No sales data found"));
-		System.out.println("The worst month for Model 3 was: " + worstMonth.date);
-		System.out.println("");
-	}
-
-	public void generateModelSReport() {
-		  List<Sales> modelSSales = salesData.getModelSSales();
-
-		  // Group the sales by year and month
-		  Map<YearMonth, Integer> monthlySales = modelSSales.stream()
-		      .collect(Collectors.groupingBy(sale -> YearMonth.parse(sale.date, DATE_FORMAT),
-		                                     Collectors.summingInt(Sales::getQuantity)));
-
-		  System.out.println("Model S Yearly Sales Report");
-		  System.out.println("---------------------------");
-		  // Print the sales by year
-		  Map<Integer, Integer> yearlySales = monthlySales.entrySet().stream()
-		      .collect(Collectors.groupingBy(entry -> entry.getKey().getYear(),
-		                                     Collectors.summingInt(Map.Entry::getValue)));
-		  for (Map.Entry<Integer, Integer> entry : yearlySales.entrySet()) {
-		    System.out.println(entry.getKey() + " -> " + entry.getValue());
-		  }
-		  System.out.println("");
-		  // Find the best and worst month
-		  Sales bestMonth = modelSSales.stream().max(Comparator.comparingInt(Sales::getQuantity))
-		      .orElseThrow(() -> new RuntimeException("No sales data found"));
-		  System.out.println("The best month for Model 3 was: " + bestMonth.date);
-
-		  Sales worstMonth = modelSSales.stream().min(Comparator.comparingInt(Sales::getQuantity))
-		      .orElseThrow(() -> new RuntimeException("No sales data found"));
-		  System.out.println("The worst month for Model 3 was: " + worstMonth.date);
-		  System.out.println("");
-		}
+        Sales worstMonth = sales.stream().min(Comparator.comparingInt(Sales::getQuantity))
+                .orElseThrow(() -> new RuntimeException("No sales data found"));
+        System.out.println("The worst month for " + model + " was: " + worstMonth.date);
+        System.out.println();
+    }
 
 
-	public void generateModelXReport() {
-		  List<Sales> modelXSales = salesData.getModelSSales();
+    public static void main(String[] args) throws IOException {
+        SalesData salesData = new SalesData();
+        salesData.readData();
 
-		  // Group the sales by year and month
-		  Map<YearMonth, Integer> monthlySales = modelXSales.stream()
-		      .collect(Collectors.groupingBy(sale -> YearMonth.parse(sale.date, DATE_FORMAT),
-		                                     Collectors.summingInt(Sales::getQuantity)));
-
-		  System.out.println("Model X Yearly Sales Report");
-		  System.out.println("---------------------------");
-		  // Print the sales by year
-		  Map<Integer, Integer> yearlySales = monthlySales.entrySet().stream()
-		      .collect(Collectors.groupingBy(entry -> entry.getKey().getYear(),
-		                                     Collectors.summingInt(Map.Entry::getValue)));
-		  for (Map.Entry<Integer, Integer> entry : yearlySales.entrySet()) {
-		    System.out.println(entry.getKey() + " -> " + entry.getValue());
-		  }
-		  System.out.println("");
-		  // Find the best and worst month
-		  Sales bestMonth = modelXSales.stream().max(Comparator.comparingInt(Sales::getQuantity))
-		      .orElseThrow(() -> new RuntimeException("No sales data found"));
-		  System.out.println("The best month for Model X was: " + bestMonth.date);
-
-		  Sales worstMonth = modelXSales.stream().min(Comparator.comparingInt(Sales::getQuantity))
-		      .orElseThrow(() -> new RuntimeException("No sales data found"));
-		  System.out.println("The worst month for Model X was: " + worstMonth.date);
-		  System.out.println("");
-		}
-
-	public static void main(String[] args) throws IOException {
-		SalesData salesData = new SalesData();
-		salesData.readData();
-
-		SalesAnalysis salesAnalysis = new SalesAnalysis(salesData);
-		salesAnalysis.generateModel3Report();
-		salesAnalysis.generateModelSReport();
-		salesAnalysis.generateModelXReport();
-	}
+        SalesAnalysis analysis = new SalesAnalysis();
+        analysis.generateReport("Model 3", salesData.getModel3Sales(), salesData);
+        analysis.generateReport("Model S", salesData.getModelSSales(), salesData);
+        analysis.generateReport("Model X", salesData.getModelXSales(), salesData);
+    }
 }
